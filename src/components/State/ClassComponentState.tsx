0571c2de63af934b543d595eac2915cd061051fb
goni715 <halesgoni715149123@gmail.com>
2024-10-06 08:22:45 +0600

    project-setup

---
+++ src/components/State/ClassComponentState.tsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+
+
+class ClassComponentState extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>Class Component</h1>
+            </div>
+        );
+    }
+}
+
+
+export default ClassComponentState;
