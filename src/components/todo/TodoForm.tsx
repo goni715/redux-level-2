ab987be7e9b20cc19c391f5a168a26be312d2b99
goni715 <halesgoni715149123@gmail.com>
2024-10-09 11:19:47 +0600

    useReducer in context provider

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -2,8 +2,8 @@ import { useContext } from "react";
 import { TodoContext } from "../../context/TodoProvider";
 
 const TodoForm = () => {
-    const {name} = useContext(TodoContext);
-    console.log(name);
+    const {state, dispatch} = useContext(TodoContext);
+    console.log(state);
     return (
         <>
             <h1 className="text-center font-semibold text-violet-500 py-3">This is Todo Form using Context Api</h1>
--- src/context/TodoProvider.tsx
+++ src/context/TodoProvider.tsx
@@ -1,15 +1,44 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useReducer } from "react";
 
-export const TodoContext = createContext(undefined);
+export const TodoContext = createContext< {state: TTodo[], dispatch:React.Dispatch<TAction>} | undefined >(undefined);
+
+type TTodo = {
+    id: string;
+    title: string;
+    isCompleted: boolean;
+}
+
+type TAction = {
+    type: string;
+    payload: TTodo
+}
+
+const initialState: TTodo[] = [];
+
+const reducer = (currentState: TTodo[], action : TAction) => {
+    switch (action.type){
+        case 'addTodo':
+            return [ ...currentState, action.payload]
+        default:
+            return currentState;
+    }
+}
 
 type TTodoProviderProps = {
     children : ReactNode;
 }
 
 const TodoProvider = ({children}: TTodoProviderProps) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const values = {
+        state,
+        dispatch
+    }
+
     return (
         <>
-             <TodoContext.Provider value={{name: 'Goni'}}>
+             <TodoContext.Provider value={values}>
                  {children}
              </TodoContext.Provider>
         </>
